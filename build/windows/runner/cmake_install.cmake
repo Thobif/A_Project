72005d40579b0a3a4bb456b33e0e06a801544284
Thobi <jirayoot537@gmail.com>
2023-06-18 14:07:01 +0700

    first commit

--- build/windows/runner/cmake_install.cmake
+++ build/windows/runner/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/fluttertest/TT/windows/runner
+# Install script for directory: C:/fluttertest/A/windows/runner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
